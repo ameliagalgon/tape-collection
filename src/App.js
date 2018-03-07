bcf413d10edda5b069442bd73205797be9847279
Amelia Galgon <agalgon@gmail.com>
2018-03-07 18:29:47 -0500

    get user access token and display basic data

--- src/App.js
+++ src/App.js
@@ -1,61 +1,125 @@
 import React, { Component } from 'react';
 import gear from './images/gear.png';
 import './App.css';
+import Request from 'superagent';
+
+import SpotifyWebApi from 'spotify-web-api-js'
+//import User from './components/User';
+
+const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
+
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    console.log(params);
+    const token = params.access_token;
+    if(token){
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: ''}
+    }
+  }
+
+  getHashParams(){
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying(){
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: {
+            name: response.item.name,
+            albumArt: response.item.album.images[0].url
+          }
+        })
+      })
+  }
+
   render() {
     return (
-      <div class="content">
-        <div class="tape-player">
-          <div class="tape-body">
-            <div class="tape-content">
-              <div class="tape-label">
-                <div class="artist">Artist name</div>
-                <div class="album">Album name</div>
+      <div className='App'>
+        <a href="http://localhost:8888">Login to Spotify</a>
+        <div>
+          Now Playing: { this.state.nowPlaying.name }
+        </div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
+        </div>
+        { this.state.loggedIn &&
+          <button onClick={() => this.getNowPlaying()}>
+            Check Now Playing
+          </button>
+        }
+      </div>
+    );
+  }
+    /*
+    return (
+      <div className="content">
+        <div className="tape-player">
+          <div className="tape-body">
+            <div className="tape-content">
+              <div className="tape-label">
+                <div className="artist">Artist name</div>
+                <div className="album">Album name</div>
               </div>
-              <div class="tape-gears">
-                <div class="gear"><img src={gear} /></div>
-                <div class="gear"><img src={gear} /></div>
+              <div className="tape-gears">
+                <div className="gear"><img src={gear} alt="gear"/></div>
+                <div className="gear"><img src={gear} alt="gear"/></div>
               </div>
             </div>
             <div id="trapezoid"></div>
           </div>
-          <div class="playback-controls">
-            <div class="button" id="play">
-              <div class="triangle-forward"></div>
+          <div className="playback-controls">
+            <div className="button" id="play">
+              <div className="triangle-forward"></div>
             </div>
-            <div class="button" id="pause">
-              <div class="pause-rectangle"></div>
-              <div class="pause-rectangle"></div>
+            <div className="button" id="pause">
+              <div className="pause-rectangle"></div>
+              <div className="pause-rectangle"></div>
             </div>
-            <div class="button" id="skip-back">
-              <div class="triangle-backward"></div><div class="triangle-backward"></div>
+            <div className="button" id="skip-back">
+              <div className="triangle-backward"></div><div className="triangle-backward"></div>
             </div>
-            <div class="button" id="skip-forward">
-              <div class="triangle-forward"></div><div class="triangle-forward"></div>
+            <div className="button" id="skip-forward">
+              <div className="triangle-forward"></div><div className="triangle-forward"></div>
             </div>
           </div>
         </div>
 
-        <div class="menu">
-          <div class="menu-content">
-            <div class="ion-home">
-              <div class="label">Home</div>
+        <div className="menu">
+          <div className="menu-content">
+            <div className="ion-home">
+              <div className="label">Home</div>
             </div>
-            <div class="ion-ios-recording">
-              <div class="label">Collection</div>
+            <div className="ion-ios-recording">
+              <div className="label">Collection</div>
             </div>
-            <div class="ion-search">
-              <div class="label">Search</div>
+            <div className="ion-search">
+              <div className="label">Search</div>
             </div>
-            <div class="ion-checkmark">
-              <div class="label">Recommendations</div>
+            <div className="ion-checkmark">
+              <div className="label">Recommendations</div>
             </div>
           </div>
         </div>
       </div>
     );
   }
+  */
 }
 
 export default App;

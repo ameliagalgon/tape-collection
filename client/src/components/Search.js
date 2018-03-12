1b9dc7338d9d5d73e23a4a89cac8a7f5a84ae630
Amelia Galgon <agalgon@gmail.com>
2018-03-12 19:27:38 -0400

    create search bar

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 class Search extends Component{
+  constructor(){
+    super();
+    this.state= {
+      query: ''
+    };
+  }
+
+  handleChange(event) {
+    this.setState({query: event.target.value});
+  }
+
+  handleSearch(event){
+    alert('Query: ' + this.state.query);
+    event.preventDefault();
+  }
 
   render(){
     return(
-      <h1>Search</h1>
+      <div className="Search">
+        <h1>Search</h1>
+        <form onSubmit={this.handleSearch} onChange={this.handChange}>
+          <input type="search" name="query" placeholder="Search..." />
+          <input type="submit" value="Search" />
+        </form>
+      </div>
     );
   }
 }
